31d453cdafbdc4a9f84d48686a8c6af5c6a9eca5
gonzalez-cathaysa <cathay.gonzalez@gmail.com>
2024-10-22 17:28:14 +0200

    Using host elements like regular elements

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,11 @@ import { TrafficComponent } from "./dashboard/traffic/traffic.component";
 import { TicketsComponent } from "./dashboard/tickets/tickets.component";
 import { DashboardItemComponent } from "./dashboard/dashboard-item/dashboard-item.component";
 
+// Every component has a host element, the element that is target by the component selector
+// It is important to understand that you select here is actually rendered to the DOM 
+// (what you select in the selector)
+// That there is a app control element in the dom of the page that's visited by the browser
+
 @Component({
   selector: 'app-root',
   standalone: true,
--- src/app/shared/button/button.component.ts
+++ src/app/shared/button/button.component.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
 
+
+
+
+
+
 @Component({
   //Prefix your custom attributes 
   selector: 'button[appButton]',
