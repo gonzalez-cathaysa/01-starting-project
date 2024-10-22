c14893b2d8e6e38f8f06ccdc4e10b1052adb52d8
gonzalez-cathaysa <cathay.gonzalez@gmail.com>
2024-10-22 17:51:04 +0200

    Interacting with host elements form inside components

--- src/app/shared/control/control.component.ts
+++ src/app/shared/control/control.component.ts
@@ -1,12 +1,20 @@
 import { Component, input, ViewEncapsulation } from '@angular/core';
 
+// go to our ControlComponent and there we can add another setting
+// to the component decorator, another configuration property, and this is the host property
+//Host wants an object as a value and that oject then takes any key value pairs of your choice
+// And that will add this class attribute to app-control wherever it's being used
+
 @Component({
   selector: 'app-control',
   standalone: true,
   imports: [],
   templateUrl: './control.component.html',
   styleUrl: './control.component.css',
-  encapsulation: ViewEncapsulation.None
+  encapsulation: ViewEncapsulation.None,
+  host: {
+    class: 'control'
+  }
 })
 export class ControlComponent {
   label = input.required<string>();
