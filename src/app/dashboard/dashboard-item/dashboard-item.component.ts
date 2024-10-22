0b30fb676f753aa4550dc31ef989bc76a24b4f2b
gonzalez-cathaysa <cathay.gonzalez@gmail.com>
2024-10-22 20:39:12 +0200

    When not to rely on host elements

--- src/app/dashboard/dashboard-item/dashboard-item.component.ts
+++ src/app/dashboard/dashboard-item/dashboard-item.component.ts
@@ -1,4 +1,10 @@
 import { Component, input, Input } from '@angular/core';
+import { of } from 'rxjs';
+
+// And again use that setting to add the class property
+// Use that setting to add the class property to the host element
+// so to this app-dashboard-item element and set it to a value of
+// dashboard-item or whichever CSS class you want to add here ...
 
 @Component({
   selector: 'app-dashboard-item',
@@ -6,6 +12,9 @@ import { Component, input, Input } from '@angular/core';
   imports: [],
   templateUrl: './dashboard-item.component.html',
   styleUrl: './dashboard-item.component.css',
+  // host: {
+  //   class: 'dashboard-item'
+  // }
 })
 export class DashboardItemComponent {
   //Expect to get an object as a value image
