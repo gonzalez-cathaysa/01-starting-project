306bf665cd236543b3fca07f173fe7d704faf387
gonzalez-cathaysa <cathay.gonzalez@gmail.com>
2024-10-24 22:07:44 +0200

    Manipulating state and using literal values

--- src/app/dashboard/server-status/server-status.component.ts
+++ src/app/dashboard/server-status/server-status.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NavigationCancellationCode } from '@angular/router';
 
 @Component({
   selector: 'app-server-status',
@@ -8,5 +9,24 @@ import { Component } from '@angular/core';
   styleUrl: './server-status.component.css'
 })
 export class ServerStatusComponent {
-  currentStatus = 'online';
+  // currentStatus = 'online';
+  // Setting specific string values as types uses a TS feature
+  // called 'Literal types'. The idea is to only allow specific
+  // (string) values - instead of all strings
+
+  currentStatus: 'online'| 'offline' | 'unknown' ='offline';
+
+  constructor(){
+    setInterval(()=>{
+      const rnd = Math.random();
+      
+      if(rnd<0.5){
+        this.currentStatus='online';
+      } else if(rnd<0.9){
+        this.currentStatus='offline';
+      } else {
+        this.currentStatus ='unknown';
+      }
+    }, 5000)
+  }
 }
