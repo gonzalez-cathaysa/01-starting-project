ad15ec080d36179d5a176aede42875d7d4293963
gonzalez-cathaysa <cathay.gonzalez@gmail.com>
2024-10-25 15:39:25 +0200

    Handling form submission repetition

--- src/app/dashboard/server-status/server-status.component.ts
+++ src/app/dashboard/server-status/server-status.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, DestroyRef, inject, OnDestroy, OnInit } from '@angular/core';
 import { NavigationCancellationCode } from '@angular/router';
+import { interval } from 'rxjs';
 
 @Component({
   selector: 'app-server-status',
@@ -8,7 +9,7 @@ import { NavigationCancellationCode } from '@angular/router';
   templateUrl: './server-status.component.html',
   styleUrl: './server-status.component.css',
 })
-export class ServerStatusComponent implements OnInit{
+export class ServerStatusComponent implements OnInit, OnDestroy{
   // currentStatus = 'online';
   // Setting specific string values as types uses a TS feature
   // called 'Literal types'. The idea is to only allow specific
@@ -16,10 +17,23 @@ export class ServerStatusComponent implements OnInit{
 
   currentStatus: 'online' | 'offline' | 'unknown' = 'offline';
 
+  // To inject a special value into your components, a so called DestroyRef
+  // you can inject it with help of the constructor, the type is destroyRef
+  // and by injecting or storing it in a property, you can set up a listener 
+  // with help of that property and the injected value that will trigger a 
+  // function whenever the component into which you injected destroyRef
+  // is about to be destroyed
+  private destroyRef = inject(DestroyRef);
+
+
+
   constructor() {}
 
   ngOnInit() {
-    setInterval(() => {
+    const interval = setInterval(() => {
+      // We could now simply store our interval in a constant, which is only available in ngOnInit,
+      // because right after setting that interval we can use DestroyRef to call OnDestroy and register
+      // a function that will be executed
       const rnd = Math.random();
 
       if (rnd < 0.5) {
@@ -30,5 +44,14 @@ export class ServerStatusComponent implements OnInit{
         this.currentStatus = 'unknown';
       }
     }, 5000);
+    this.destroyRef.onDestroy(()=> {
+      clearInterval(interval)
+    });
+    //Register a function that will be executed by angular when this component is 
+    //about to be destroyed. That interval constant is only available in ngOnInit,
+    //but that's also where I'm setting up this Destroy listener, and you can use 
+    //it as many OnDestroy listeners as you need in any methods of this component
+    
   }
+
 }
